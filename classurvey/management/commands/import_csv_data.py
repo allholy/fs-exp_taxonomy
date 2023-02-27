bfd6b51787c5edf6156ddfbc7cb79b727ab4d2ae
allholy <pennyan19@gmail.com>
2023-02-27 15:24:36 +0100

    import csv data for sounds to database

---
+++ classurvey/management/commands/import_csv_data.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand, CommandError
+from classurvey.models import TestSound
+
+import csv
+import os
+
+# path of data file as an argument
+# your databse in a csv
+class Command(BaseCommand):
+    help = 'Path for file with data about the test sounds.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('path', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        path = options['path'][0]
+        if not os.path.exists(path):
+            self.stderr.write(self.style.ERROR(f"The path '{path}' does not exist."))
+            return
+            
+        self.stdout.write(self.style.SUCCESS(f"Importing data from '{path}'."))
+
+        import_sounds_csv(path)
+
+
+# import data
+def import_sounds_csv(file_path):
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            sound = TestSound.objects.create(
+                sound_id=row['ID'],
+                sound_class=row['Class']
+            )
--- classurvey/models.py
+++ classurvey/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class TestSound(models.Model):
+    sound_id = models.CharField(max_length=50)
+    sound_class = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.sound_id
+    
