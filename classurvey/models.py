528665f3a93ee200430749a805a2ee0952e884b2
allholy <pennyan19@gmail.com>
2023-03-13 14:57:58 +0100

    add all categories

--- classurvey/models.py
+++ classurvey/models.py
@@ -10,16 +10,45 @@ class TestSound(models.Model):
         return f"<TestSound {self.sound_id}>"
     
 
-# store data 
+# store data
 class SoundAnswer(models.Model):
     # should be foreign key
     user_id = models.CharField(max_length=50)  # this can be FS ID or IP ADDRESS
     test_sound = models.ForeignKey(TestSound, on_delete=models.CASCADE)
     # available choices, test for now with few
     test_choices =(
-        ("M-SOLO", "Solo"),
-        ("M-MULT", "Multi"),
-        ("FX-HOUSE", "House"),
-        ("SP-CR", "Crowd")
+        ("m-p", "Percussion"),
+        ("m-si","Solo"),
+        ("m-ms", "Multi"),
+        ("m-other", "Other"),
+
+        ("i-p","Percussion"),
+        ("i-s","Wind"),
+        ("i-w","String"),
+        ("i-t","resttt"),
+        ("i-e","Synth / Electronic"),
+        ("i-other", "Other"),
+
+        ("sp-s", "Solo"),
+        ("sp-co", "Conversation"),
+        ("sp-cr", "Crowd"),
+        ("sp-other", "Other"),
+
+        ("fx-o", "Daily Objects - House Appliances"),
+        ("fx-v", "Vehicle sounds"),
+        ("fx-m", "Other mechanisms, engines, machines"),
+        ("fx-h", "Human sounds"),
+        ("fx-a","Animals"),
+        ("fx-n","Natural occurrences"),
+        ("fx-el","Electronic - Sci-fi"),
+        ("fx-experiment","Experimental"),
+        ("fx-d","Design"),
+        ("fx-other", "Other"),
+
+        ("ss-n","Natural"),
+        ("ss-i","Indoors"),
+        ("ss-u","Urban"),
+        ("ss-s","Synthetic - Artificial"),
+        ("ss-other", "Other")
     )
     chosen_class = models.CharField(max_length=15, choices=test_choices, default="")
