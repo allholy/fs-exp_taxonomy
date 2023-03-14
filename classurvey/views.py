3b5d1ead80ebd9d090c767471fbfe0be86caa8ef
allholy <pennyan19@gmail.com>
2023-03-14 12:51:09 +0100

    add: automated random id for user bug fix: when the page reloads while error, keep same sound

--- classurvey/views.py
+++ classurvey/views.py
@@ -4,29 +4,50 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from .models import SoundAnswer, TestSound
 from .forms import SoundAnswerForm
+import random
+
+
+
+def user_id_from_request(request):
+    user_id = request.session.get("user_id", None)
+    if user_id is None:
+        user_id= "user_"+ str(random.randint(99,9999999))
+        request.session["user_id"]= user_id
+    return user_id
 
 
 def home_view(request):
+    user_id_from_request(request)
     return render(request, 'classurvey/home.html')
+
+
+def get_next_sound_for_user(user_id):
+    #if there are no more sounds, return none
+    return random.choice(TestSound.objects.all())
 
 
 #test one question
 def annotate_sound(request):
-
-    import random
-    test_sound = random.choice(TestSound.objects.all())
-
-    form = SoundAnswerForm(request.POST or None)
+    user_id = user_id_from_request(request)
 
     if request.POST:
+        form = SoundAnswerForm(request.POST)
+        test_sound = TestSound.objects.get(id=request.POST.get("test_sound_id"))
         if form.is_valid():
             sound_answer = form.save(commit=False)
             sound_answer.test_sound_id = request.POST.get("test_sound_id")
-            # TODO: save user id here 
+            sound_answer.user_id = user_id
             sound_answer.save()
             # redirect to next sound
 
             print(f"number of answers {SoundAnswer.objects.count()}")
             return HttpResponseRedirect(reverse('classurvey:main'))
-        
+
+    else:
+        form = SoundAnswerForm()
+        test_sound=get_next_sound_for_user(user_id)
+        if test_sound is None:
+            return HttpResponseRedirect(reverse('classurvey:exit'))
+
     return render(request, 'classurvey/annotate_sound.html', {'test_sound': test_sound, 'form': form})
+
