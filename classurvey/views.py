548d9b7cd81a2d3cae7063304419d9209f4b2045
allholy <pennyan19@gmail.com>
2023-03-15 16:03:01 +0100

    function for get next sound

--- classurvey/views.py
+++ classurvey/views.py
@@ -20,10 +20,27 @@ def home_view(request):
     user_id_from_request(request)
     return render(request, 'classurvey/home.html')
 
+def get_next_sound_for_user(request, group_number):
+    '''
+    Retrieve the sounds that belong to a group and each time return one random
+    sound until no more sound are remaining. 
+    '''
+    user_id = user_id_from_request(request)
 
-def get_next_sound_for_user(user_id):
-    #if there are no more sounds, return none
-    return random.choice(TestSound.objects.all())
+    remaining_sounds = TestSound.objects.filter(sound_group=group_number)
+
+    passed_sound_ids = request.session.get('passed_sound_ids', [])
+
+    remaining_sounds = remaining_sounds.exclude(id__in=passed_sound_ids)
+
+    if not remaining_sounds:
+        return None
+    else:
+        next_sound = random.choice(remaining_sounds)
+        passed_sound_ids.append(next_sound.id)
+        request.session[f'passed_sound_ids'] = passed_sound_ids
+
+        return next_sound
 
 
 #test one question
