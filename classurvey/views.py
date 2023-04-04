dc304d2db42f287170e47d1ed96c2879aa1d66cf
allholy <pennyan19@gmail.com>
2023-04-04 23:44:38 +0200

    minor code formatting

--- classurvey/views.py
+++ classurvey/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from .models import SoundAnswer, TestSound
@@ -10,7 +10,7 @@ import random
 def user_id_from_request(request):
     user_id = request.session.get('user_id', None)
     if user_id is None:
-        user_id = 'user_'+ str(random.randint(10,9999999))
+        user_id = 'user_' + str(random.randint(10, 9999999))
         request.session['user_id'] = user_id
     return user_id
 
@@ -21,14 +21,16 @@ def get_ip_address(request):
     else:
         return request.META.get('REMOTE_ADDR')
 
-def assign_group(request,user_id):
+def assign_group(request, user_id):
     ''' 
     Assign one group to the user and save it to the session.
     '''
     # see on the given table how many groups exist
-    available_groups = TestSound.objects.values_list('sound_group', flat=True).distinct()
+    available_groups = TestSound.objects.values_list(
+        'sound_group', flat=True).distinct()
     # groups done. if it is empty, none are done.
-    groups_already_done = SoundAnswer.objects.filter(user_id=user_id).values_list('test_sound__sound_group', flat=True).distinct()
+    groups_already_done = SoundAnswer.objects.filter(user_id=user_id).values_list(
+        'test_sound__sound_group', flat=True).distinct()
     print(groups_already_done)
 
     if not len(groups_already_done) == len(available_groups):
@@ -49,16 +51,20 @@ def get_next_sound_for_user(request):
     user_id = user_id_from_request(request)
     group_number = request.session['group_number']
 
-    test_sound_ids_in_group = TestSound.objects.filter(sound_group=group_number).values_list('id', flat=True)
-    test_sound_ids_already_answered = SoundAnswer.objects.filter(test_sound_id__in=test_sound_ids_in_group, user_id=user_id).values_list('test_sound_id', flat=True)
+    test_sound_ids_in_group = TestSound.objects.filter(
+        sound_group=group_number).values_list('id', flat=True)
+    test_sound_ids_already_answered = SoundAnswer.objects.filter(
+        test_sound_id__in=test_sound_ids_in_group, user_id=user_id).values_list('test_sound_id', flat=True)
 
-    remaining_sounds = TestSound.objects.filter(sound_group=group_number).exclude(id__in=test_sound_ids_already_answered)
+    remaining_sounds = TestSound.objects.filter(
+        sound_group=group_number).exclude(id__in=test_sound_ids_already_answered)
 
     if not remaining_sounds:
         return None
     else:
         next_sound = random.choice(remaining_sounds)
         return next_sound
+
 
 def sounds_sizes(request):
     '''
@@ -67,9 +73,11 @@ def sounds_sizes(request):
     user_id = user_id_from_request(request)
     group_number = request.session['group_number']
 
-    test_sound_ids_in_group = TestSound.objects.filter(sound_group=group_number).values_list('id', flat=True)
-    test_sound_ids_already_answered = SoundAnswer.objects.filter(test_sound_id__in=test_sound_ids_in_group, user_id=user_id).values_list('test_sound_id', flat=True)
-    
+    test_sound_ids_in_group = TestSound.objects.filter(
+        sound_group=group_number).values_list('id', flat=True)
+    test_sound_ids_already_answered = SoundAnswer.objects.filter(
+        test_sound_id__in=test_sound_ids_in_group, user_id=user_id).values_list('test_sound_id', flat=True)
+
     total_sounds_size = len(test_sound_ids_in_group)
     answered_sounds_size = len(test_sound_ids_already_answered)
     return total_sounds_size, answered_sounds_size
@@ -131,13 +139,14 @@ def annotate_sound_view(request):
     else:
         form = SoundAnswerForm()
         test_sound = get_next_sound_for_user(request)
+        # request.session['next_sound'] = test_sound
         if test_sound is None:
             return redirect(reverse('classurvey:exit_info'))
 
     return render(request, 'classurvey/annotate_sound.html', {
         'test_sound': test_sound, 'form': form,
         'all_sounds_size': all_sounds_size, 'answered_sounds_size': answered_sounds_size,
-        })
+    })
 
 def exit_info_view(request):
     if request.method == 'POST':
