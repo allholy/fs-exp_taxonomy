94dd7d234a90ab777975a98ad3f5a0c8fd02c888
allholy <pennyan19@gmail.com>
2023-04-06 12:50:03 +0200

    import choices for sound answer from csv

--- classurvey/models.py
+++ classurvey/models.py
@@ -12,9 +12,20 @@ class TestSound(models.Model):
         return f"<TestSound {self.sound_id}>"
 
 
-# store data
-class SoundAnswer(models.Model):
+# store input data for sounds
+import csv
+def csv_to_tuple_choices(csv_file):
+    '''
+    Import test choices (key and display name) from csv file.
+    '''
+    with open(csv_file, 'r') as f:
+        reader = csv.reader(f)
+        labels = next(reader)
+        data = [(row[0], row[1]) for row in reader]
+    tuple_choices = tuple(data)
+    return(tuple_choices)
 
+class SoundAnswer(models.Model):
     user_id = models.CharField(max_length=50)  # random generate
     test_sound = models.ForeignKey(TestSound, on_delete=models.CASCADE)
     date_created = models.DateTimeField('Creation date', auto_now_add=True) #timezone aware
@@ -23,41 +34,7 @@ class SoundAnswer(models.Model):
     # to change them manually in annotate_sound.html file.
 
     # available choices
-    test_choices =(
-        ("m-p", "Percussion"),
-        ("m-si","Solo"),
-        ("m-ms", "Multi"),
-        ("m-other", "Other"),
-
-        ("i-p","Percussion"),
-        ("i-s","Wind"),
-        ("i-w","String"),
-        ("i-t","resttt"),
-        ("i-e","Synth / Electronic"),
-        ("i-other", "Other"),
-
-        ("sp-s", "Solo"),
-        ("sp-co", "Conversation"),
-        ("sp-cr", "Crowd"),
-        ("sp-other", "Other"),
-
-        ("fx-o", "Daily Objects - House Appliances"),
-        ("fx-v", "Vehicle sounds"),
-        ("fx-m", "Other mechanisms, engines, machines"),
-        ("fx-h", "Human sounds"),
-        ("fx-a","Animals"),
-        ("fx-n","Natural occurrences"),
-        ("fx-el","Electronic - Scifi"),
-        ("fx-exp","Experimental"),
-        ("fx-d","Design"),
-        ("fx-other", "Other"),
-
-        ("ss-n","Natural"),
-        ("ss-i","Indoors"),
-        ("ss-u","Urban"),
-        ("ss-s","Synthetic - Artificial"),
-        ("ss-other", "Other")
-    )
+    test_choices = csv_to_tuple_choices('classurvey/data/choices.csv')
     chosen_class = models.CharField(max_length=15, choices=test_choices, default="")
     likert_choices = ((1, 'Strongly Unconfident'), (2, 'Unconfident'), (3, 'Neutral'), (4, 'Confident'), (5, 'Strongly Confident'))
     confidence = models.IntegerField(choices=likert_choices,default="")
