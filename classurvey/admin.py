f3accc86b5be1e119e6065b77334b0e820fc4944
allholy <pennyan19@gmail.com>
2023-05-09 15:42:12 +0200

    create admin fields

--- classurvey/admin.py
+++ classurvey/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class SoundAnswerAdmin(admin.ModelAdmin):
+    list_display = ('user_id','chosen_class','confidence','date_created')
+    list_filter = ['date_created']
+
+class ExitInfoAdmin(admin.ModelAdmin):
+    list_display = ('user_id','date_created')
+    search_fields = ['answer']
+
+
+# given data
+admin.site.register(models.ClassChoice)
+admin.site.register(models.TestSound)
+
+# user data
+admin.site.register(models.SoundAnswer, SoundAnswerAdmin)
+admin.site.register(models.ExitInfoModel, ExitInfoAdmin)
