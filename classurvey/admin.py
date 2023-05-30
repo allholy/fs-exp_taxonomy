ee457dd7b1a9813e137610dc2f7204e098af270d
allholy <pennyan19@gmail.com>
2023-05-30 11:53:26 +0200

    admin changes for given data

--- classurvey/admin.py
+++ classurvey/admin.py
@@ -15,10 +15,16 @@ class UserDetailsAdmin(admin.ModelAdmin):
     list_display = ('user_id','ip_address','q1','q2','q3','q4','date_created')
     search_fields = ['answer']
 
+class TestSoundAdmin(admin.ModelAdmin):
+    list_display = ('sound_id','sound_class','sound_group','sound_difficulty','sound_name')
+
+class ClassChoiceAdmin(admin.ModelAdmin):
+    list_display = ('class_key','class_name','top_level','description','examples')
+
 
 # given data
-admin.site.register(models.ClassChoice)
-admin.site.register(models.TestSound)
+admin.site.register(models.ClassChoice, ClassChoiceAdmin)
+admin.site.register(models.TestSound, TestSoundAdmin)
 
 # user data
 admin.site.register(models.SoundAnswer, SoundAnswerAdmin)
